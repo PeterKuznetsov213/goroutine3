cc3cbf4cf81d154f80bfce14bc8ae147a414faaf
PeterKuznetsov213 <peterkuznetsov213@gmail.com>
2022-10-19 14:26:42 +0400

    Реализована разбивка строки на слова

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	numbers := "one two three four five six seven eight nine ten"
+	chan1 := make(chan string)
+	chan2 := make(chan string)
+	go goroutineReformat(chan1, numbers)
+	go goroutineFilter(chan1, chan2)
+	goroutinePrint(chan2)
+}
+func goroutineReformat(downstream chan string, numbers string) {
+	stringSlice := strings.Fields(numbers)
+	for value := range stringSlice {
+		downstream <- stringSlice[value]
+	}
+	close(downstream)
+}
+
+func goroutineFilter(upstream, downstream chan string) {
+	for value := range upstream {
+		if value != "" {
+			downstream <- value
+		}
+
+	}
+	close(downstream)
+}
+
+func goroutinePrint(upstream chan string) {
+	for value := range upstream {
+		fmt.Println(value)
+	}
+}
